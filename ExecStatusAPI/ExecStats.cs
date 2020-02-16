cdc7f83a36adc02acaf2d5790c6a0a4bc24de101
Lanz <lanza@wwt.com>
2020-02-16 14:40:02 -0800

    Add all API functionality

--- ExecStatusAPI/ExecStats.cs
+++ ExecStatusAPI/ExecStats.cs
@@ -3,32 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExecStatusAPI
 {
-    class ExecStats
+    public class ExecStats
     {
         public ExecStats(string id, string sourcemachine, string appname, string task,
-                           int status, int prop1, int prop2, int prop3)
+                            int status, int Prop1, int Prop2, int Prop3)
         {
             Id = id;
             SourceMachine = sourcemachine;
             AppName = appname;
             Task = task;
             Status = status;
-            Prop1 = prop1;
-            Prop2 = prop2;
-            Prop3 = prop3;
+            prop1 = Prop1;
+            prop2 = Prop2;
+            prop3 = Prop3;
+            
         }
-        public string Id { get; set; }
-
-        public string SourceMachine { get; set; }
-        public string AppName { get; set; }
-        public string Task { get; set; }
-        public int Status { get; set; }
-        public int Prop1 { get; set; }
-        public int Prop2 { get; set; }
-        public int Prop3 { get; set; }
 
         public void SetCurrentTime()
         {
@@ -36,19 +29,18 @@ namespace ExecStatusAPI
             Id = ((int)time.TotalSeconds).ToString();
         }
 
-        public string GetMessage()
-        {
-            string message = "{\"Id\": " + Id + ","
-               + "\"SourceMachine\": " + SourceMachine + ","
-               + "\"AppName\": " + AppName + ","
-               + "\"Task\": " + Task + ","
-               + "\"Status\": " + Status + ","
-               + "\"prop1\": " + Prop1 + ","
-               + "\"prop2\": " + Prop2 + ","
-               + "\"prop3\": " + Prop3 + "}";
+        [Required]
+        public string Id { get; set; }
 
-            return message;
-        }
+        public DateTime Time { get; set; }
+        public string SourceMachine { get; set; }
+        public string AppName { get; set; }
+        public string Task { get; set; }
+        public int Status { get; set; }
+        public int prop1 { get; set; }
+        public int prop2 { get; set; }
+        public int prop3 { get; set; }
+
     }
     
 }
--- ExecStatusAPI/ExecStatusApp.cs
+++ ExecStatusAPI/ExecStatusApp.cs
@@ -1,60 +1,164 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ExecStatusAPI
 {
     class ExecStatusApp
     {
-        public string API_ENDPOINT = "https://execstatusapp20191220061912.azurewebsites.net/api/";
-        public string app_exec_stat_get()
+        static HttpClient client = new HttpClient();
+        
+        static void ShowExecStats(ExecStats stats)
         {
-            WebRequest wrGetURL;
-            wrGetURL = WebRequest.Create(API_ENDPOINT + "ExecStats");
-
-            Stream data = wrGetURL.GetResponse().GetResponseStream();
-            StreamReader data_reader = new StreamReader(data);
-
-            string message = data_reader.ReadToEnd();
-
-            data.Close();
-            return message;
+            if (stats != null)
+            {
+                Console.WriteLine(
+                   $"AppName: {stats.AppName}\n" +
+                   $"Id: {stats.Id}\n" +
+                   $"SourceMachine: {stats.SourceMachine}\n" +
+                   $"Status: {stats.Status}\n" +
+                   $"Task: {stats.Task}\n" +
+                   $"Time: {stats.Time}\n" +
+                   $"prop1: {stats.prop1}\n" +
+                   $"prop2: {stats.prop2}\n" +
+                   $"prop3: {stats.prop3}\n"
+                   );
+            }
         }
 
-        public string app_exec_stat_get_id(string id)
+        static async Task<Uri> CreateExecStatsAsync(ExecStats stats)
         {
-            WebRequest wrGetURL;
-            wrGetURL = WebRequest.Create(API_ENDPOINT + "ExecStats/" + id);
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "ExecStats", stats);
+            response.EnsureSuccessStatusCode();
 
-            Stream data = wrGetURL.GetResponse().GetResponseStream();
-            StreamReader data_reader = new StreamReader(data);
-
-            string message = data_reader.ReadToEnd();
-
-            data.Close();
-            return message;
+            // return URI of the created resource.
+            return response.Headers.Location;
         }
 
-        /*
-         public string app_exec_stat_post(ExecStats data)
-         {
-
-             var request = (HttpWebRequest)WebRequest.Create(API_ENDPOINT + "ExecStats");
-             request.ContentType = "application/json";
-             request.Method = "POST";
-
-             StreamWriter wr_stream = new StreamWriter(request.GetRequestStream());
-             wr_stream.Write(data.GetMessage());
-
-             HttpWebResponse response = (HttpWebResponse) request.GetResponse();
-             StreamReader r_stream = new StreamReader(response)
-             
+        static async Task<List<ExecStats>> GetExecStatsAsync()
+        {
+            string path = "ExecStats";
+            List<ExecStats> stats = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                stats = await response.Content.ReadAsAsync<List<ExecStats>>();
+            }
+            return stats;
         }
-        */
+
+        static async Task<ExecStats> GetExecStatsAsync_Id(string Id)
+        {
+            string path = $"ExecStats/{Id}";
+            ExecStats stats = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                stats = await response.Content.ReadAsAsync<ExecStats>();
+            }
+           
+            return stats;
+        }
+
+        static async Task<ExecStats> UpdateExecStatsAsync(ExecStats stats)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"ExecStats/{stats.Id}", stats);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            stats = await response.Content.ReadAsAsync<ExecStats>();
+            return stats;
+        }
+
+        static async Task<HttpStatusCode> DeleteExecStatsAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"ExecStats/{id}");
+            return response.StatusCode;
+        }
+
+        static async Task<List<ExecStats>> GetExecStatsActivityAsync_Id(string Id)
+        {
+            string path = $"ExecStatsActivity/{Id}";
+            List<ExecStats> stats = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                stats = await response.Content.ReadAsAsync<List<ExecStats>>();
+            }
+            
+            return stats;
+        }
+
         static void Main()
         {
-            ExecStatusApp test = new ExecStatusApp();
-            test.app_exec_stat_get();
+            RunAsync().GetAwaiter().GetResult();
         }
+
+        //Enter API calls here
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://execstatusapp.azurewebsites.net/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            try
+            {
+                ExecStats stats1 = new ExecStats("1580505409", "machine1", "appname1", "task1",0, 1, 2, 3);
+                ExecStats stats2 = new ExecStats("1580505410", "machine2", "appname2", "task2", 0, 1, 2, 3);
+
+                Console.WriteLine("PUT 2 ITEMS");
+                var url = await CreateExecStatsAsync(stats1);
+                Console.WriteLine($"Created at {url}");
+                url = await CreateExecStatsAsync(stats2);
+                Console.WriteLine($"Created at {url}");
+
+                Console.WriteLine("GET ALL ITEMS");
+                List<ExecStats> all_stats = null;
+                all_stats = await GetExecStatsAsync();
+                foreach(var i in all_stats)
+                {
+                    ShowExecStats(i);
+                }
+                
+                Console.WriteLine("\nGET ITEMS BY ID");
+                ExecStats stats = null;
+                stats = await GetExecStatsAsync_Id("1580505409");
+                ShowExecStats(stats);
+
+                Console.WriteLine("\nGET ITEMS BY TASK");
+                all_stats = null;
+                all_stats = await GetExecStatsActivityAsync_Id("task2");
+                foreach (var i in all_stats)
+                {
+                    ShowExecStats(i);
+                }
+
+                Console.WriteLine("DELETE ALL");
+                var statuscode1 = await DeleteExecStatsAsync(stats1.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statuscode1})");
+                var statuscode2 = await DeleteExecStatsAsync(stats2.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statuscode2})");
+
+                Console.WriteLine("done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+            
+        }
+
+        
+        
     }
 }
