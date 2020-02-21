aebe64132d7d7916654cc23ef0b9dfeec600054e
Lanz <lanza@wwt.com>
2020-02-21 11:19:35 -0800

    Update .ps1 function names

--- ExecStatusApp.ps1
+++ ExecStatusApp.ps1
@@ -6,18 +6,18 @@
 $URL_ENDPOINT = 'https://execstatusapp.azurewebsites.net/api/ExecStats'
 
 #get request
-function app_exec_stat_get {
+function Get-AppExecStat {
 	Invoke-RestMethod -Uri $URL_ENDPOINT
 }
 
 #get request by id
-function app_exec_stat_get_id {
+function Get-AppExecStatId {
     param([string] $id)
     Invoke-RestMethod -Uri "$URL_ENDPOINT/$id"
 }
 
 #post request (note that the default Id is the current time)
-function app_exec_stat_post {
+function Post-AppExecStat {
     param([string] $id = ([int] (Get-Date (Get-Date).ToUniversalTime() -UFormat %s)).ToString(), 
     [string] $AppName = "N/A", [string] $SourceMachine = "N/A", [string] $Status = "0", 
     [string] $Task = "N/A", [string] $prop1 = "1", [string] $prop2 = "2", [string] $prop3 = "3")
@@ -47,13 +47,13 @@ function app_exec_stat_post {
 }
 
 #get request by task 
-function app_exec_stat_get_activity {
+function Get-AppExecStatActivity {
     param([string] $task)
     Invoke-RestMethod -Uri $URL_ENDPOINT'Activity/'$task
 }
 
 #delete request
-function app_exec_stat_delete {
+function Delete-AppExecStat {
     param([string] $id)
     Invoke-RestMethod -Uri "$URL_ENDPOINT/$id" -Method Delete
 }
@@ -63,22 +63,22 @@ function app_exec_stat_delete {
 
 #test post method
 echo "`nTest Post method"
-app_exec_stat_post -id "1234567" -AppName "app1" -SourceMachine "machine1" -Status "0" `
+Post-AppExecStat -id "1234567" -AppName "app1" -SourceMachine "machine1" -Status "0" `
 -Task "task1" -prop1 "1" -prop2 "2" -prop3 "3"
 
-app_exec_stat_post -id "12345678" -AppName "app2" -SourceMachine "machine2" -Status "0" `
+Post-AppExecStat -id "12345678" -AppName "app2" -SourceMachine "machine2" -Status "0" `
 -Task "task2" -prop1 "1" -prop2 "2" -prop3 "3"
 
 #test get method
 echo "`nGet all items"
-app_exec_stat_get
+Get-AppExecStat
 
 #test get by task
 echo "`nGet by task"
-app_exec_stat_get_activity -task "task2"
+Get-AppExecStatActivity -task "task2"
 
 #delete entries
 echo "`nDelete entry"
-app_exec_stat_delete -id "1234567"
-app_exec_stat_delete -id "12345678"
+Delete-AppExecStat -id "1234567"
+Delete-AppExecStat -id "12345678"
 
